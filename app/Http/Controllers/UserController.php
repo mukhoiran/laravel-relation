ffbbb3c42dda140e2ab3195f2299d129d8cc483b
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-06-16 20:51:28 +0700

    first view

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+  public function show($id)
+  {
+      return view('user.profile', ['user' => User::findOrFail($id)]);
+  }
+}
