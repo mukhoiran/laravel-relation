e5d37bf137f05fe9edb9db563af2994cc94b2f07
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-07-18 03:08:09 +0700

    create table lessons and lesson_user

---
+++ database/migrations/2019_07_17_200236_create_lesson_user_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLessonUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lesson_user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('lesson_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')
+                  ->on('users')->onDelete('cascade');
+
+            $table->foreign('lesson_id')->references('id')
+                  ->on('lessons')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lesson_user');
+    }
+}
